47fa06388bc50d711f6930623b3980df550bd53e
Iskeletu <iskeletu.br@gmail.com>
2022-07-15 18:22:59 -0300

    Minor changes for first release.

--- Source/__init__.py
+++ Source/__init__.py
@@ -1,16 +1,1 @@
-__version__ = '0.0.1'
-
-from Source.HAF import *
-from Source.Constants import *
-from Source.TemplateCreator import *
-
-from Source.CLI.CommandLineInterface import *
-
-from Source.Driver.DriverHandler import *
-
-from Source.FileHandler.Config import *
-from Source.FileHandler.JsonHandler import *
-from Source.FileHandler.Logger import *
-
-from Source.Ticket.AttachentHandler import *
-from Source.Ticket.TicketHandler import *
+__version__ = '0.alpha.1'
