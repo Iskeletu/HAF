7e117b4076bf4a7d9facc62a18b6ebe44865e7a7
Iskeletu <iskeletu.br@gmail.com>
2022-07-16 10:06:38 -0300

    Added setup files.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'HAF',
+    version = '0.alpha.1',
+    author = 'Fábio Gandini',
+    author_email = 'iskeletu.br@gmail.com',
+    description = 'Helix AutoFiller ticket management tool.'
+)
