a108262b75f42fe80f50d4a9557c53a1bd134a7f
Iskeletu <iskeletu.br@gmail.com>
2022-07-19 14:28:44 -0300

    Changed project directory anchor.

--- HAF/Constants.py
+++ HAF/Constants.py
@@ -44,7 +44,8 @@ class Paths:
     Group of path related global variables.
     * Uses double undescore to specify private methods instead of the convenional single underscore.
     """
-    __PROJECT_DIRECTORY = pathlib.Path().parent.resolve().parent.resolve()
+
+    __PROJECT_DIRECTORY = pathlib.Path().parent.parent.resolve()
     
     ATTACHMENTS_FOLDER_PATH = f'{__PROJECT_DIRECTORY}\\Autofill Dictionary\\Attachments\\'
     DICTIONARY_JSON_PATH = f'{__PROJECT_DIRECTORY}\\Autofill Dictionary\\dictionary.json'
